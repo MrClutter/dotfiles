376df41e91c2a1fb8c073cb20bed655400088c0b
MrClutter <63581238+MrClutter@users.noreply.github.com>
2024-05-17 20:51:58 +0200

    Create kick-start.sh

---
+++ kick-start.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+# VSCode 
+#{
+#  "key": "f8",
+#  "command": "workbench.action.terminal.runSelectedText",
+#  "when": "editorTextFocus && editorLangId == 'shellscript'"
+#}
+
+dystro=$(uname -a)
+
+if [[ $dystro == *"Ubuntu"* ]]; then
+    declare -a appList=(
+        wget
+        curl
+        neovim
+        xclip
+        zoxide
+        tmux
+        zsh
+        python
+        tldr
+        scc
+        exa
+        fastfetch
+        duf # disk usage
+        entr # Watch for changes and run command
+        exiftool
+        fzf
+    )
+    sudo apt-get update && sudo apt-get upgrade
+
+    for package in "${appList[@]}"; do
+        echo "Installing $package"
+        sudo apt-get install "$package" -y
+        echo ""
+    done
+
+    sudo apt install $a
+    echo "aaaa"
+fi
+
+
+#if [ $dystro -eq ''
